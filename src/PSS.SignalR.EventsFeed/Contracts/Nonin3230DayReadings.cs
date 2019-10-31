1e4aa9742f93a4d0c269b2c64b9bcb3ad33e92f4
Kailash Ravuri <kailu.ravuri@gmail.com>
2019-10-31 11:55:11 +0000

    Add contracts

---
+++ src/PSS.SignalR.EventsFeed/Contracts/Nonin3230DayReadings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PAREXEL.PSS.SignalR.EventsFeed.Contracts
+{
+    public class Nonin3230DayReadings
+    {
+        public string DataItemId { get; set; }
+        public string MessageType { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string DaySplit { get; set; }
+        public SortedDictionary<DateTimeOffset, PulseOximeterReading> PulseOximeterReadings{ get; set; }
+    }
+}
---
+++ src/PSS.SignalR.EventsFeed/Contracts/PulseOximeterReading.cs
@@ -0,0 +1,10 @@
+﻿namespace PAREXEL.PSS.SignalR.EventsFeed.Contracts
+{
+    public class PulseOximeterReading
+    {
+        public int SpO2 { get; set; }
+        public int PulseRate { get; set; }
+        public string TrackingId { get; set; }
+        public string TimeZone { get; set; }
+    }
+}
